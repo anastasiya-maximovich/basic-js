3280afdf0cad7586616e7cc506f8ff0543c6cb7e
anastasiya-maximovich <anastasiya025@gmail.com>
2020-09-30 01:30:44 +0300

    feat: what-season

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
+module.exports = function getSeason(date) {
+   if(date === null){
+    return 'Unable to determine the time of year!';
+  }
+  else if(date !== number){
+    return 'Error';
+  }
   throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+  else {
+    
+
+  }
+
+  }
+
+ 

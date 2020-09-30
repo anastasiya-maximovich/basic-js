1f9ee58506ad71d70c06591534fdb337c9f08bcb
anastasiya-maximovich <anastasiya025@gmail.com>
2020-10-01 01:38:17 +0300

    feat: add dream team. have 2 failing still

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,12 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if(!Array.isArray(members) || members === null){
+    return false;
+  }
+  else{
+    let newArray = members.filter(member => typeof(member) === 'string');
+     
+     return newArray = members.map(member => member.trim()[0].toLocaleUpperCase()).sort().join('') || false;
+  }
 };

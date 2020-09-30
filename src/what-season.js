8ec660dcef62ce7e973d580cef9f5d5136491546
anastasiya-maximovich <anastasiya025@gmail.com>
2020-09-30 15:27:34 +0300

    fix: fix seasons test

--- src/what-season.js
+++ src/what-season.js
@@ -2,28 +2,27 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason(date) {
 
-   if(date === null){
+   if(!date){
     return 'Unable to determine the time of year!';
   }
-  //else if(date !== ){
-  //  return 'Error';
- // }
+  else if (Object.prototype.toString.call(date) !== '[object Date]'){
+    throw Error;
+  }// этот кусок честно списала
+  //мой вариант был if(date !== Date), как-то так
 
   else {
     let month = date.getMonth();
-    if(month === 3-5){
+    if(month >= 3 && month <= 5){
       return 'spring';
     }
-    else if(month === 6-8){
+    else if(month >= 6 && month <= 8){
       return 'summer';
     }
-    else if(month === 9-11){
+    else if(month >= 9 && month <= 11){
       return 'autumn (fall)';
     }
-    else(month === 0 || month === 11-12){
+    else(month >= 0 || month <= 2)
       return 'winter';
-    }
-
   }
 
   }

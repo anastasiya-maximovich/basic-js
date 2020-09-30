19668a2408291aa80af0a4b366bca4f50726c066
anastasiya-maximovich <anastasiya025@gmail.com>
2020-09-30 14:51:25 +0300

    feat: add what-season

--- src/what-season.js
+++ src/what-season.js
@@ -1,15 +1,28 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason(date) {
+
    if(date === null){
     return 'Unable to determine the time of year!';
   }
-  else if(date !== number){
-    return 'Error';
-  }
-  throw new CustomError('Not implemented');
+  //else if(date !== ){
+  //  return 'Error';
+ // }
+
   else {
-    
+    let month = date.getMonth();
+    if(month === 3-5){
+      return 'spring';
+    }
+    else if(month === 6-8){
+      return 'summer';
+    }
+    else if(month === 9-11){
+      return 'autumn (fall)';
+    }
+    else(month === 0 || month === 11-12){
+      return 'winter';
+    }
 
   }
 

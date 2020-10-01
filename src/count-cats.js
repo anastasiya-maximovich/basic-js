988c47d8d50db56c0b7b7a20dffc0a22382b7e88
anastasiya-maximovich <anastasiya025@gmail.com>
2020-10-01 03:38:52 +0300

    feat: delit cats,

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,8 +1,6 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function countCats (matrix) {
-  let newMatrix = matrix.concat();
-  let newCats = newMatrix.filter(cats => '^^');
-  return newCats.length;
+  
 }
 

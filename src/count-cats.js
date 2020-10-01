9f10879d6500bb7d8d44da1f1876daa4d22f5636
anastasiya-maximovich <anastasiya025@gmail.com>
2020-10-01 03:48:44 +0300

    feat: add cats. Passing all of them.

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,7 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function countCats (matrix) {
-  
+  let newMatrix = matrix.flat();
+  return newMatrix.filter(cats  => cats === "^^").length;
 }
 

adc672bf9a697ba204372f858fbd62d9a83b71c6
anastasiya-maximovich <anastasiya025@gmail.com>
2020-10-01 03:57:03 +0300

    refacto: add commits to cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,7 +1,7 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function countCats (matrix) {
-  let newMatrix = matrix.flat();
-  return newMatrix.filter(cats  => cats === "^^").length;
+  let newMatrix = matrix.flat(); // была канкатенация. В Дискорте объяснили, что она тут не сработает. Флат нашла сама
+  return newMatrix.filter(cats  => cats === "^^").length; // в дискорте указали на ошибку в записи фильтра. Ошибку исправила сама
 }
 

7faaab9b0d2b95219ec078d2e1ae1c3108049753
Anastasiya Maximovich <63050839+anastasiya-maximovich@users.noreply.github.com>
2021-02-09 15:28:26 +0300

    feat: fix cats
    fix cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,10 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats (matrix) {
-  let newMatrix = matrix.flat(); // была канкатенация. В Дискорте объяснили, что она тут не сработает. Флат нашла сама
-  return newMatrix.filter(cats  => cats === "^^").length; // в дискорте указали на ошибку в записи фильтра. Ошибку исправила сама
-}
+module.exports = function countCats( matrix ) {
+  let cats=0;
+  for(let i=0; i < matrix.length; i++) 
+    for( let j=0; j < matrix[i].length; j++)  
+      if(matrix[i][j] === '^^') cats++;
+  
+ return cats;
+};
